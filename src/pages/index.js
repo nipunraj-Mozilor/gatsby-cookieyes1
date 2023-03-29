5ba010ba6e4fe4e11c262cc0607e24bf57d620f8
nipunraj <nipunraj99@gmail.com>
2023-03-29 12:48:16 +0530

    cookies added

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,24 @@ const IndexPage = () => {
       <h1>Welcome to my Gatsby site!</h1>
       <Link to='/about'>About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <iframe
+        width='560'
+        height='315'
+        src='https://www.youtube.com/embed/lLfCvi6KVok'
+        title='YouTube video player'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowfullscreen
+      ></iframe>
+      <h4>Vimeo</h4>
+      <iframe
+        src='https://player.vimeo.com/video/198661221?h=37f11c0160&title=0&byline=0&portrait=0'
+        width='640'
+        height='360'
+        frameborder='0'
+        allow='autoplay; fullscreen; picture-in-picture'
+        allowfullscreen
+      ></iframe>
     </main>
   );
 };
