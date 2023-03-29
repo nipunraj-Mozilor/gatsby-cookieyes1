93ded5eaaee08254a2e8d922a736c5f03722d9b7
nipunraj <nipunraj99@gmail.com>
2023-03-29 12:35:30 +0530

    used 2nd code method

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,10 @@
-const headComponents = [
-  <script
-    id='cookieyes'
-    type='text/javascript'
-    src='https://cdn-cookieyes.com/client_data/4a1165be30e4e56d0c6f7765/script.js'
-  ></script>,
-];
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents(headComponents);
-};
+import React from 'react';
+export function onRenderBody({ setHeadComponents }) {
+  setHeadComponents([
+    <script
+      id='cookieyes'
+      type='text/javascript'
+      src='https://cdn-cookieyes.com/client_data/4a1165be30e4e56d0c6f7765/script.js'
+    ></script>,
+  ]);
+}
