c84938f69079021357511d41da9feb4dae88c82d
nipunraj <nipunraj99@gmail.com>
2023-03-29 13:39:45 +0530

    stack

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@ module.exports = {
     title: `.`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-gdpr-cookies`,
+      options: {
+        googleAnalytics: {
+          trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+          // Setting this parameter is optional
+          anonymize: true,
+        },
+        facebookPixel: {
+          pixelId: 'YOUR_FACEBOOK_PIXEL_ID',
+        },
+        // Defines the environments where the tracking should be available  - default is ["production"]
+        environments: ['production', 'development'],
+      },
+    },
+  ],
+};
